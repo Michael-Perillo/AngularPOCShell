bf78a254e6bd7fbcfe66c485ef9931e101e39302
Michael-Perillo <michael@perillo.me>
2025-06-02 17:47:56 -0400

    Finalize local shell mfe setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { FeaturesComponent } from './features/features.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'features', component: FeaturesComponent },
-  { path: '**', redirectTo: '' }, // Redirect any unknown paths to home
+  {
+    path: 'remote',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module', // Use 'module' for native module federation
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './ViewsModule',
+      }).then(m => m.ViewsModule),
+  },
+  // { path: '**', redirectTo: '' }, // Redirect any unknown paths to home
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 
