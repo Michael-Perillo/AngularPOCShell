3afb919a96914deedd132522efd0ff1c9819b5aa
Michael-Perillo <michael@perillo.me>
2025-06-12 18:49:06 -0400

    Updated to use angular Meta

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { REMOTE_ENTRY_MM } from './shared/constants/prod/constants';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,11 @@ import { NavigationComponent } from './navigation/navigation.component';
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class App {
   protected title = 'app';
+  constructor(private meta: Meta) {
+    const remoteEntry = REMOTE_ENTRY_MM.replace(/\/?remoteEntry\.js$/, '');
+    this.meta.updateTag({
+      'http-equiv': 'Content-Security-Policy',
+      content: `script-src 'self' 'unsafe-eval' ${remoteEntry}; object-src 'self'`,
+    });
+  }
 }
