e83940e489bbb51470da09879c81848afa020dab
Michael-Perillo <michael@perillo.me>
2025-06-12 16:59:23 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+const { REMOTE_ENTRY_MM } = require('./src/app/shared/constants/prod/constants.ts');
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mfe1": "http://localhost:4201/remoteEntry.js",
+    "mfe1": REMOTE_ENTRY_MM,
   },
 
   shared: {
