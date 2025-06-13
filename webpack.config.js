ab0b61010211c64b0940901018405afe38e3979f
Michael-Perillo <michael@perillo.me>
2025-06-12 20:25:26 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = withModuleFederationPlugin({
   },
 
   shared: {
+    '@angular/core': { singleton: true, strictVersion: true },
+    '@angular/common': { singleton: true, strictVersion: true },
+    '@angular/router': { singleton: true, strictVersion: true },
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
 
